234252b7295f0b1bad3c0e4cbb8b89be6455bf8d
Mikong Galero <mgalero@engineyard.com>
2016-02-23 11:26:03 -0800

    Update express to latest and basic app accordingly.

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var express = require('express');
-var app     = express.createServer();
-var port    = process.env.PORT || 3000;
+var app = express();
+var port = process.env.PORT || 3000;
 
-app.get('/', function(request, response) {
-  response.send('Hello Engine Yard Cloud!');
+app.get('/', function (req, res) {
+  res.send('Hello Engine Yard Cloud!')
 });
 
 app.listen(port);
